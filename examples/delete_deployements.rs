c8a401d32824ece2b6285a26914c79e3e5dfdb78
Luke Sanderson <luke.sanderson@mongodb.com>
2025-09-02 11:36:58 +0100

    Add delete_deployment example

---
+++ examples/delete_deployements.rs
@@ -0,0 +1,22 @@
+use anyhow::{Context, Result};
+use atlas_local::Client;
+use bollard::Docker;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let docker = Docker::connect_with_socket_defaults().context("connecting to docker")?;
+    let client = Client::new(docker);
+
+    client
+        .delete_deployment("local1234")
+        .await
+        .context("Deleting atlas local container local1234")?;
+    println!("local1234 successfully deleted");
+
+    client
+        .delete_deployment("other_none_local_atlas")
+        .await
+        .context("Attempting to delete other non-atlas container")?;
+
+    Ok(())
+}
