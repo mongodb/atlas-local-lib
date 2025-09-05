d8869c7e2fbad96fc778463750d12d6e9d95f334
Luke Sanderson <luke.sanderson@mongodb.com>
2025-09-05 11:22:10 +0100

    Improve e2e file structure and add new tests

---
+++ tests/e2e_test_delete.rs
@@ -0,0 +1,106 @@
+mod e2e_test_utils;
+use atlas_local::{
+    Client, DeleteDeploymentError, GetDeploymentError,
+    models::{CreateDeploymentOptions, GetLocalDeploymentLabelsError, IntoDeploymentError},
+};
+use bollard::{Docker, query_parameters::ListContainersOptions};
+
+use crate::e2e_test_utils::{
+    DOCKER_TEST_MUTEX, TestContainerCleaner, create_persistent_unrelated_container,
+};
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_delete_only_deletes_atlas_local() {
+    // Acquire the global lock to ensure isolation for docker tests
+    // If another test panics, the lock may be poisoned but we still want to run the tests
+    let _guard = DOCKER_TEST_MUTEX.lock().unwrap_or_else(|e| e.into_inner());
+    let mut container_cleaner = TestContainerCleaner::new();
+
+    let docker = Docker::connect_with_socket_defaults().unwrap();
+    let client = Client::new(docker);
+
+    // Create a deployment
+    let deployment_name = "test_deployment_name";
+    container_cleaner.add_container(deployment_name);
+    let deployment1 = CreateDeploymentOptions {
+        name: Some(deployment_name.to_string()),
+        ..Default::default()
+    };
+    client
+        .create_deployment(&deployment1)
+        .await
+        .expect("Creating deployment");
+
+    // Create a dummy container
+    let docker = Docker::connect_with_socket_defaults().unwrap();
+    let dummy_container_name = "dummy-container";
+    container_cleaner.add_container(dummy_container_name);
+    create_persistent_unrelated_container(&docker, dummy_container_name)
+        .await
+        .expect("Creating dummy container");
+
+    // We are using docker directly rather than client because we want to list all containers not just deployments
+    let containers = docker
+        .list_containers(None::<ListContainersOptions>)
+        .await
+        .expect("Listing containers");
+    assert_eq!(containers.len(), 2);
+
+    // Delete the deployment
+    client
+        .delete_deployment(deployment_name)
+        .await
+        .expect("Deleting deployment");
+    // Attempt to delete the dummy container
+    let delete_result = client.delete_deployment(dummy_container_name).await;
+
+    // Check Error is of the correct type
+    match delete_result {
+        Err(DeleteDeploymentError::GetDeployment(GetDeploymentError::IntoDeployment(
+            IntoDeploymentError::LocalDeploymentLabels(
+                GetLocalDeploymentLabelsError::NotALocalDeployment,
+            ),
+        ))) => {
+            // If we reach this block, the assertion has passed.
+        }
+        Err(e) => {
+            panic!(
+                "Deleting the dummy container did not return the expected error. Instead, it returned: {:?}",
+                e
+            );
+        }
+        Ok(_) => {
+            panic!("Deletion of dummy container was successful when it should fail");
+        }
+    }
+
+    // Check only the deployment was deleted and the alpine container was not
+    let containers = docker
+        .list_containers(None::<ListContainersOptions>)
+        .await
+        .expect("Listing containers");
+    assert_eq!(containers.len(), 1);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_deletion_deployment_does_not_exist() {
+    let _guard = DOCKER_TEST_MUTEX.lock().unwrap_or_else(|e| e.into_inner());
+
+    let docker = Docker::connect_with_socket_defaults().unwrap();
+    let client = Client::new(docker);
+
+    // Delete a deployment without creating it first
+    let delete_result = client.delete_deployment("does-not-exist").await;
+
+    // Check Error is of the correct type
+    match delete_result {
+        Err(DeleteDeploymentError::GetDeployment(GetDeploymentError::ContainerInspect(_))) => {
+            // If we reach this block, the assertion has passed.
+        }
+        _ => {
+            panic!(
+                "The delete result did not return the expected DeleteDeploymentError::GetDeployment(GetDeploymentError::ContainerInspect(_))."
+            );
+        }
+    }
+}
---
+++ tests/e2e_test_utils.rs
@@ -0,0 +1,91 @@
+use bollard::{
+    Docker,
+    query_parameters::{
+        CreateContainerOptionsBuilder, CreateImageOptionsBuilder, RemoveContainerOptionsBuilder,
+        StartContainerOptions,
+    },
+    secret::ContainerCreateBody,
+};
+use futures_util::StreamExt;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+use tokio::runtime::Handle;
+
+// Mutex that ensures e2e tests that create deployments are completed in isolation
+// Important for avoiding naming conflicts and for counting how many docker containers were created
+pub static DOCKER_TEST_MUTEX: Lazy<Mutex<i32>> = Lazy::new(|| Mutex::new(0));
+
+#[derive(Default)]
+pub struct TestContainerCleaner {
+    container_names: Vec<String>,
+}
+
+impl TestContainerCleaner {
+    pub fn new() -> Self {
+        let container_names = Vec::new();
+        Self { container_names }
+    }
+
+    pub fn add_container(&mut self, name: &str) {
+        self.container_names.push(name.to_string());
+    }
+}
+
+// Runs when TestContainerCleaner goes out of scope at either end of test or panic
+impl Drop for TestContainerCleaner {
+    fn drop(&mut self) {
+        let docker = Docker::connect_with_defaults().unwrap();
+        let runtime_handle = Handle::current();
+
+        // Blocks current thread to ensure no new tests are started until these containers are cleaned up
+        tokio::task::block_in_place(move || {
+            runtime_handle.block_on(async {
+                // Removes all containers created during this test
+                for container_name in &self.container_names {
+                    let _ = docker
+                        .remove_container(
+                            container_name,
+                            Some(RemoveContainerOptionsBuilder::default().force(true).build()),
+                        )
+                        .await;
+                }
+            })
+        });
+    }
+}
+
+pub async fn create_persistent_unrelated_container(
+    docker: &Docker,
+    name: &str,
+) -> Result<(), bollard::errors::Error> {
+    const ALPINE_IMAGE: &str = "alpine";
+    const ALPINE_TAG: &str = "3.18.10";
+    // Create Alpine image
+    let create_image_options = CreateImageOptionsBuilder::default()
+        .from_image(ALPINE_IMAGE)
+        .tag(ALPINE_TAG)
+        .build();
+
+    // Start pulling the image, which returns a stream of progress events
+    let mut stream = docker.create_image(Some(create_image_options), None, None);
+
+    // Iterate over the stream and check for errors
+    while let Some(result) = stream.next().await {
+        let _image_info = result?;
+    }
+
+    // Create Alpine Container
+    let options = Some(CreateContainerOptionsBuilder::default().name(name).build());
+    let config = ContainerCreateBody {
+        image: Some(format!("{ALPINE_IMAGE}:{ALPINE_TAG}").to_string()),
+        cmd: Some(vec!["sleep".to_string(), "3600".to_string()]),
+        ..Default::default()
+    };
+    docker.create_container(options, config).await?;
+
+    // Start Container
+    docker
+        .start_container(name, None::<StartContainerOptions>)
+        .await?;
+    Ok(())
+}
