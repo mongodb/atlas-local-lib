1ad14a1942c0bee8c1f2dfc77ef8293212f64f5b
Melanija Cvetic <melanija.cvetic@mongodb.com>
2025-09-12 16:12:11 +0100

    Checking behaviour when workflow not in container

--- src/client/get_connection_string.rs
+++ src/client/get_connection_string.rs
@@ -35,7 +35,7 @@ impl<D: DockerInspectContainer> Client<D> {
 
         // Construct the connection string
         let connection_string = format_connection_string(req.db_username, req.db_password, port);
-        print!("Connection String: {}", connection_string);
+        // print!("Connection String: {}", connection_string);
 
         // Optionally, verify the connection string
         if req.verify.unwrap_or(false) {
