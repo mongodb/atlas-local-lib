6db5a5597fabbd65add8b39e56e5db2631ec814b
Luke Sanderson <luke.sanderson@mongodb.com>
2025-09-17 12:04:56 +0100

    Update CI

--- tests/e2e_test.rs
+++ tests/e2e_test.rs
@@ -73,6 +73,7 @@ async fn test_e2e_smoke_test() {
         name: Some(name.to_string()),
         mongodb_initdb_root_username: Some(username.to_string()),
         mongodb_initdb_root_password: Some(password.to_string()),
+        port_bindings: Some(MongoDBPortBinding::new(Some(50000), BindingType::AnyInterface)),
         ..Default::default()
     };
     client
